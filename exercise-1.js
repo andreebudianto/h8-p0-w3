846cf9e4f121549b58362e58d045b286ed99c50f
budiandree <benedictandree@gmail.com>
2019-07-08 22:09:40 +0700

    add exercise-1

---
+++ exercise-1.js
@@ -0,0 +1,13 @@
+//input "hello world!"
+//output
+"!dlrow olleh"
+var c = '';
+function balikString(teks) {
+    var b = teks.length;
+    for(a=0;a<(teks.length); a++) {
+        var b = b - 1;        
+        c = c + teks[b];        
+    } console.log(c);
+}
+
+balikString('hello world!');
