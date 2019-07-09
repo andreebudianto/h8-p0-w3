c0d4f24c14b6ea3b6308b60f1b1bc57cb98381d8
andree budianto <50732824+andreebudianto@users.noreply.github.com>
2019-07-09 23:00:18 +0700

    Update exercise-4.js

--- exercise-4.js
+++ exercise-4.js
@@ -1,5 +1,4 @@
-//contoh output
-["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]
+
 var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
 function dataHandling2(input) {
 
