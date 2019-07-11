9da2b8340d730ece6ac6434274dd44aa62781ce9
budiandree <benedictandree@gmail.com>
2019-07-11 10:54:51 +0700

    del sandbox, add exercise 8

--- exercise-6.js
+++ exercise-6.js
@@ -1,10 +1,71 @@
 function angkaPalindrome(num) {
+    
+    function revNum(basenum) {
+        var reversedNum = '';
+        var step = String(basenum).length;
+        for(var i=0; i<String(basenum).length; i++) {
+            reversedNum = reversedNum + String(basenum)[step-1];
+            step--;
+        } 
+        return reversedNum;
+    }
+
+    function nextPalindromeSameBaseEvenLengthNumber(num) {
+        var convStr = String(num);
+        var step = String(num).length;
+        var nextP = '';
+        var result = '';
+        
+        for(var i=0; i<(Math.floor(Number(step/2))); i++){
+            nextP = nextP + convStr[i];
+        } result = nextP + revNum(nextP);
+        return result;
+    }
+    
+    function nextPalindromeSameBaseOddLengthNumber(num) {
+        var convStr = String(num);
+        var step = String(num).length;
+        var nextP = '';
+        var result = '';
+        
+        for(var i=0; i<(Math.floor(Number(step/2))); i++){
+            nextP = nextP + convStr[i];
+        } result = nextP + convStr[(Math.floor(Number(step/2)))] +revNum(nextP);
+        return result;
+    }
+    
+    function nextPalindromeIncrementBaseEvenLengthNumber(num) {
+        var convStr = String(num);
+        var step = String(num).length;
+        var nextP = '';
+        var result = '';
+        
+        for(var i=0; i<(Math.floor(Number(step/2))); i++){
+            nextP = nextP + convStr[i];
+        } nextP = Number(nextP) +1;
+        result = nextP + revNum(nextP);
+        return result;
+    }
+    
+    function nextPalindromeIncrementBaseOddLengthNumber(num) {
+        var convStr = String(num);
+        var step = String(num).length;
+        var nextP = '';
+        var result = '';
+        
+        for(var i=0; i<(Math.floor(Number(step/2))); i++){
+            nextP = nextP + convStr[i];
+        } 
+        result = nextP + String(Number(convStr[(Math.floor(Number(step/2)))])+1) + revNum(nextP);
+        return result;
+    }
+    
     if(num <9) {
         return num+1;
     } else if (num == 9) {
         return num +2;    
     } else if (num > 9) {
-        if (String(num).length%2 == 0) {
+        if((String(num).length)%2 === 0) {
             nextPalindromeSameBaseEvenLengthNumber(num);
             
             if(Number(nextPalindromeSameBaseEvenLengthNumber(num)) > num) {
@@ -22,84 +83,6 @@ function angkaPalindrome(num) {
             }
         }
     }
-}
-
-function revNum(basenum) {
-    var reversedNum = '';
-    var step = String(basenum).length;
-    for(var i=0; i<String(basenum).length; i++) {
-        reversedNum = reversedNum + String(basenum)[step-1];
-        step--;
-    } 
-    return reversedNum;
-}
-
-function nextPalindromeSameBaseEvenLengthNumber(num) {
-    var convStr = String(num);
-    var step = String(num).length;
-    var nextP = '';
-    var result = '';
-    
-    for(var i=0; i<(Math.floor(Number(step/2))); i++){
-        nextP = nextP + convStr[i];
-    } result = nextP + revNum(nextP);
-    return result;
-}
-
-function nextPalindromeSameBaseOddLengthNumber(num) {
-    var convStr = String(num);
-    var step = String(num).length;
-    var nextP = '';
-    var result = '';
-    
-    for(var i=0; i<(Math.floor(Number(step/2))); i++){
-        nextP = nextP + convStr[i];
-    } result = nextP + convStr[(Math.floor(Number(step/2)))] +revNum(nextP);
-    return result;
-}
-
-function nextPalindromeIncrementBaseEvenLengthNumber(num) {
-    var convStr = String(num);
-    var step = String(num).length;
-    var nextP = '';
-    var result = '';
-    
-    for(var i=0; i<(Math.floor(Number(step/2))); i++){
-        nextP = nextP + convStr[i];
-    } nextP = Number(nextP) +1;
-    result = nextP + revNum(nextP);
-    return result;
-}
-
-function nextPalindromeIncrementBaseOddLengthNumber(num) {
-    var convStr = String(num);
-    var step = String(num).length;
-    var nextP = '';
-    var result = '';
-    
-    for(var i=0; i<(Math.floor(Number(step/2))); i++){
-        nextP = nextP + convStr[i];
-    } 
-    result = nextP + String(Number(convStr[(Math.floor(Number(step/2)))])+1) + revNum(nextP);
-    return result;
-}
-
-function pickFirstHalf(num) {
-    var result = '';
-    var convStr = String(num);
-    for(var i=0; i<(String(num).length)/2; i ++) {
-        result = result + convStr[i]
-    } return result;
-}
-
-function pickLastHalf(num) {
-    var result = '';
-    var convStr = String(num);
-    var step = String(num).length/2;
-    for(var i=0; i<(String(num).length)/2; i ++) {
-        result = result + convStr[step];
-        step++;
-    } return result;
 }
 // TEST CASES
   console.log(angkaPalindrome(8)); // 9
